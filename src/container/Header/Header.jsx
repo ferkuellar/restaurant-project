b3ac02a8a62dca1aab30f99542988d4e6d1c200b
Fernando Cuellar <fercuellar@gmail.com>
2023-06-09 15:15:53 -0600

    se arreglaron los errores de la pagina de about us y header y se finalizo la seccion

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,20 +5,19 @@ import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-	<div className='app__header app__wrapper section__padding' id = 'home'>
-
-		<div className='app__wrapper_info'>
-			<SubHeading title= 'Persigue el Sabor' />
-			<h1 className='app__header-h1'>La Llave para una Deliciosa Cena</h1>
-			<p className = 'p__opensans' style={{ margin: '2rem 0'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut fringilla est, eu auctor risus. Sed fringilla sed justo vel porttitor. Etiam vel dolor fermentum, dignissim dui ut, luctus orci. Donec vel massa blandit, molestie nisl non, condimentum urna. </p>
-			<button type = 'button' className= 'custom__button'>Explorar Menù</button>
-		</div>
-
-		<div className = 'app__wrapper_image'>
-			<img src={images.welcome} alt='header_image' className='header__img'/>
+	<div className="app__header app__wrapper section__padding" id="home">
+		<div className="app__wrapper_info">
+			<SubHeading title="Persigue el Nuevo sabor" />
+			<h1 className="app__header-h1">La Llave Para una cena deliciosa</h1>
+			<p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+			<button type="button" className="custom__button">Explorar Menù</button>
 		</div>
 	
-	</div>
+		<div className="app__wrapper_img">
+			<img src={images.welcome} alt="header_img" />
+		</div>
+		</div>
 );
+
 
 export default Header;
